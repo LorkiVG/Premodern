914bcdbaf4cfd8fef9a72a9f4fb7a0936b50a38b
LorkiNikita <69603120+LorkiNikita@users.noreply.github.com>
2021-05-06 22:33:22 +0300

    Add files via upload

---
+++ public/backend/routes/admin/urls.php
@@ -0,0 +1,14 @@
+<?php 
+return [
+  ['admin','admin','index'],
+  ['admin/login','admin','login'],
+  ['admin/logout', 'admin', 'logout'],
+  ['admin/statistic' , 'admin','statistic'],
+  ['admin/players' , 'admin','players'],
+  ['admin/active' , 'admin','active'],
+  ['admin/others', 'admin', 'others'],
+  ['admin/players/edit_player', 'admin', 'editPlayer'],
+  ['admin/players/get_skin_by_player', 'admin' , 'getSkinByPlayer']
+];
+
+
---
+++ public/backend/routes/site/urls.php
@@ -0,0 +1,23 @@
+<?php 
+
+return array (
+  ['','main','index'],
+  ['helpme','main','help'],
+  ['donate', 'merchant', 'donate'],
+  ['donate/payeer', 'merchant', 'payeer'],
+  ['donate/requisites', 'merchant', 'requisites'],
+  ['login','account','login'],
+  ['register', 'account','register'],
+  ['profile', 'account','profile'],
+  ['fpassword', 'account','fpassword'],
+  ['logout', 'account','logout'],
+  ['account/confirm', 'account' ,'confirm'],
+  ['account/new_email', 'account', 'newEmail'],
+  ['account/reset', 'account' ,'reset'],
+  ['account/reset/ajax', 'account', 'reseting'],
+  ['account/get_skin', 'account', 'getSkin'],
+  ['account/get_active_skin', 'account', 'getActiveSkin'],
+  ['upload_skin', 'account', 'uploadSkin'],
+  ['account/set_active_skin', 'account', 'setActiveSkin'],
+  
+);
